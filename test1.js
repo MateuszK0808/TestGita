c821fd44321b467fedec10d91a1b8edadfe87ed2
MateuszK0808 <mateuszk0808@interia.pl>
2017-04-26 20:12:19 +0200

    instrukcje

--- test1.js
+++ test1.js
@@ -5,3 +5,14 @@ var j = 2;
 
 console.log('UWAGA UWAGA');
 console.log('Zmieany w test 1')
+
+/*przejście na odpowiedni branch
+  rebase master
+  git pull
+  vim:
+    :i lub i  pisanie
+    escape
+    :wq       zapisanie
+    enter
+  git push
+*/
